dc48241bcdcb1860299ef9bc2a9d682aeecb909a
seancarroll <seanc28@gmail.com>
2023-12-12 22:57:44 -0600

    fix getting framework via CLI

--- lib/doctavious-cli/src/cmd/frameworks/get.rs
+++ lib/doctavious-cli/src/cmd/frameworks/get.rs
@@ -3,8 +3,14 @@ use cifrs::frameworks;
 use crate::{CliResult, DoctaviousCliError};
 
 pub fn invoke(name: String) -> CliResult<Option<String>> {
-    let framework = frameworks::get_all().into_iter().find(|f| f.name == name);
-    serde_json::to_string(&framework)
-        .map_err(DoctaviousCliError::SerdeJson)
-        .map(|f| Some(f))
+    let normalized_string = name.to_lowercase();
+    let framework = frameworks::get_all().into_iter().find(|f| {
+        f.name.to_lowercase() == normalized_string || f.id.to_lowercase() == normalized_string
+    });
+    match framework {
+        None => Ok(None),
+        Some(f) => serde_json::to_string(&f)
+            .map_err(DoctaviousCliError::SerdeJson)
+            .map(|f| Some(f)),
+    }
 }
