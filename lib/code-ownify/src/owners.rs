7c81cdfb82577034e15c2f5fb7c80d8aaf363fdd
seancarroll <seanc28@gmail.com>
2025-04-14 23:30:09 -0500

    flushing out codeowners

--- lib/code-ownify/src/owners.rs
+++ lib/code-ownify/src/owners.rs
@@ -1,13 +1,14 @@
-// location: in the .github/, root, or docs/
-// need to swap .github for provider agnostic
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
+use std::{fs, io};
 
-use std::io;
-
-use regex::Regex;
-use scm::errors::ScmError;
+use scm::providers::ScmProviders;
 use thiserror::Error;
-use tracing::{debug, info};
-use walkdir::{DirEntry, WalkDir};
+use tracing::info;
+
+use crate::parse::pattern_to_regex;
+
+const CODEOWNERS: &'static str = "codeowners";
 
 #[remain::sorted]
 #[derive(Debug, Error)]
@@ -21,6 +22,79 @@ pub enum CodeOwnersError {
 
 pub type CodeOwnersResult<T> = Result<T, CodeOwnersError>;
 
-pub struct CodeOwners {}
+pub struct CodeOwners {
+    location: PathBuf,
+    owners: HashMap<String, Vec<String>>,
+}
 
-impl CodeOwners {}
+impl CodeOwners {
+    pub fn new(location: PathBuf) -> CodeOwnersResult<Option<CodeOwners>> {
+        if location.exists() {
+            let owners = Self::parse(&location)?;
+            return Ok(Some(Self { location, owners }))
+        }
+
+        Ok(None)
+    }
+
+    pub fn discover(root: PathBuf) -> CodeOwnersResult<Option<CodeOwners>> {
+        let root_codeowners = root.join(CODEOWNERS);
+        if root_codeowners.exists() {
+            let owners = Self::parse(&root_codeowners)?;
+            return Ok(Some(Self {
+                location: root_codeowners,
+                owners
+            }));
+        }
+
+        let docs_codeowners = root.join("docs").join(CODEOWNERS);
+        if docs_codeowners.exists() {
+            let owners = Self::parse(&docs_codeowners)?;
+            return Ok(Some(Self {
+                location: docs_codeowners,
+                owners
+            }));
+        }
+
+        for dot_directory in ScmProviders::dot_directories() {
+            if dot_directory.exists() {
+                let owners = Self::parse(&dot_directory)?;
+                return Ok(Some(Self {
+                    location: dot_directory,
+                    owners
+                }));
+            }
+        }
+
+        Ok(None)
+    }
+
+    // TODO: probably belongs in parse...
+    fn parse(location: &Path) -> CodeOwnersResult<HashMap<String, Vec<String>>> {
+        let mut owners = HashMap::new();
+        for line in fs::read_to_string(&location)?.lines() {
+            let trimmed_line = line.trim();
+            if trimmed_line.is_empty() || trimmed_line.starts_with("#") {
+                continue;
+            }
+            let fields: Vec<String> = line.split_whitespace().map(str::to_string).collect();
+            if fields.len() == 1 {
+                info!(
+                    "expected at least two fields for rule in {}: {}",
+                    &location.to_string_lossy(),
+                    line
+                );
+                continue;
+            }
+
+            let (rule_pattern, rest) = fields.split_first().expect("Rule should have a pattern");
+            owners.insert(rule_pattern.to_string(), rest.to_vec());
+        }
+
+        Ok(owners)
+    }
+
+    pub fn render(&self, format: &'static str) -> CodeOwnersResult<String> {
+        todo!()
+    }
+}
