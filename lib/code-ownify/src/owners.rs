def625be76a25e2599929c2748c4d832e3c1e685
seancarroll <seanc28@gmail.com>
2025-04-23 22:37:02 -0500

    tweaking codeowners

--- lib/code-ownify/src/owners.rs
+++ lib/code-ownify/src/owners.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::io::Write;
-use std::path::{PathBuf};
+use std::path::{Path, PathBuf};
 use std::{fs, io};
 
 use scm::providers::ScmProviders;
@@ -24,52 +24,112 @@ pub enum CodeOwnersError {
 pub type CodeOwnersResult<T> = Result<T, CodeOwnersError>;
 
 pub struct CodeOwners {
-    pub location: PathBuf,
+    location: PathBuf,
+    owners: HashMap<String, Vec<String>>,
 }
 
 impl CodeOwners {
-    pub fn new(location: PathBuf) -> Option<CodeOwners> {
-        if location.exists() {
-            return Some(Self { location });
-        }
-
-        None
+    pub fn new(location: PathBuf) -> CodeOwnersResult<CodeOwners> {
+        Ok(Self {
+            owners: CodeOwners::parse(&location)?,
+            location,
+        })
     }
 
-    pub fn discover(root: PathBuf) -> Option<CodeOwners> {
+    // pub fn from(path: &Path) -> CodeOwnersResult<Option<HashMap<String, Vec<String>>>> {
+    //     if path.exists() {
+    //         return Ok(Some(Self::parse(&path)?));
+    //     }
+    //
+    //     Ok(None)
+    // }
+    //
+    // pub fn d(root: &Path) -> CodeOwnersResult<Option<HashMap<String, Vec<String>>>> {
+    //     let root_codeowners = root.join(CODEOWNERS);
+    //     if root_codeowners.exists() {
+    //         return Ok(Some(Self::parse(&root_codeowners)?));
+    //     }
+    //
+    //     let docs_codeowners = root.join("docs").join(CODEOWNERS);
+    //     if docs_codeowners.exists() {
+    //         return Ok(Some(Self::parse(&docs_codeowners)?));
+    //     }
+    //
+    //     for dot_directory in ScmProviders::dot_directories() {
+    //         if dot_directory.exists() {
+    //             return Ok(Some(Self::parse(&dot_directory)?));
+    //         }
+    //     }
+    //
+    //
+    //     Ok(None)
+    // }
+
+    pub fn discover(root: PathBuf) -> CodeOwnersResult<Option<CodeOwners>> {
         let root_codeowners = root.join(CODEOWNERS);
         if root_codeowners.exists() {
-            return Some(Self {
+            return Ok(Some(Self {
+                owners: CodeOwners::parse(&root_codeowners)?,
                 location: root_codeowners,
-            });
+            }));
         }
 
         let docs_codeowners = root.join("docs").join(CODEOWNERS);
         if docs_codeowners.exists() {
-            return Some(Self {
+            return Ok(Some(Self {
+                owners: CodeOwners::parse(&docs_codeowners)?,
                 location: docs_codeowners,
-            });
+            }));
         }
 
         for dot_directory in ScmProviders::dot_directories() {
             if dot_directory.exists() {
-                return Some(Self {
+                return Ok(Some(Self {
+                    owners: CodeOwners::parse(&dot_directory)?,
                     location: dot_directory,
-                });
+                }));
             }
         }
 
-        None
+        Ok(None)
     }
 
-    pub fn owners(&self) -> CodeOwnersResult<HashMap<String, Vec<String>>> {
+    pub fn location(&self) -> &Path {
+        self.location.as_path()
+    }
+
+    pub fn owners(&self) -> &HashMap<String, Vec<String>> {
+        &self.owners
+    }
+
+    // pub fn owners(&self) -> CodeOwnersResult<HashMap<String, Vec<String>>> {
+    //     let mut owners = HashMap::new();
+    //     for line in fs::read_to_string(&self.location)?.lines() {
+    //         if let Some((rule_pattern, pattern_owners)) = parser::parse_line(line) {
+    //             if pattern_owners.is_empty() {
+    //                 info!(
+    //                     "expected subscribers for rule in {}: {}",
+    //                     &self.location.to_string_lossy(),
+    //                     line
+    //                 );
+    //                 continue;
+    //             }
+    //
+    //             owners.insert(rule_pattern.to_string(), pattern_owners.to_vec());
+    //         }
+    //     }
+    //
+    //     Ok(owners)
+    // }
+
+    fn parse(path: &Path) -> CodeOwnersResult<HashMap<String, Vec<String>>> {
         let mut owners = HashMap::new();
-        for line in fs::read_to_string(&self.location)?.lines() {
+        for line in fs::read_to_string(path)?.lines() {
             if let Some((rule_pattern, pattern_owners)) = parser::parse_line(line) {
                 if pattern_owners.is_empty() {
                     info!(
                         "expected subscribers for rule in {}: {}",
-                        &self.location.to_string_lossy(),
+                        path.to_string_lossy(),
                         line
                     );
                     continue;
@@ -83,7 +143,7 @@ impl CodeOwners {
     }
 
     pub fn render<W: Write>(&self, writer: &mut W, format: &'static str) -> CodeOwnersResult<()> {
-        let owners = self.owners()?;
+        let owners = self.owners();
         match format {
             "markdown" => {
                 writeln!(writer, "| File(s) | Owners |")?;
@@ -117,9 +177,9 @@ mod tests {
         let code_owners =
             CodeOwners::new(temp_dir.join("CODEOWNERS")).expect("Should have found CODEOWNER file");
 
-        let owners = code_owners.owners().unwrap();
+        let owners = code_owners.owners();
         assert_eq!(
-            HashMap::from([("**/*.md".to_string(), vec!["@markdown".to_string()])]),
+            &HashMap::from([("**/*.md".to_string(), vec!["@markdown".to_string()])]),
             owners
         );
     }
@@ -130,12 +190,14 @@ mod tests {
 
         fs::write(temp_dir.join("CODEOWNERS"), "**/*.md @markdown").unwrap();
 
-        let code_owners = CodeOwners::discover(temp_dir).expect("Should have found CODEOWNER file");
+        let code_owners = CodeOwners::discover(temp_dir)
+            .expect("Should have found CODEOWNER file")
+            .unwrap();
 
-        let owners = code_owners.owners().unwrap();
+        let owners = code_owners.owners();
 
         assert_eq!(
-            HashMap::from([("**/*.md".to_string(), vec!["@markdown".to_string()])]),
+            &HashMap::from([("**/*.md".to_string(), vec!["@markdown".to_string()])]),
             owners
         );
     }
@@ -149,12 +211,14 @@ mod tests {
         fs::write(docs_dir.join("CODEOWNERS"), "**/*.md @markdown").unwrap();
         fs::write(docs_dir.join("file.md"), "").unwrap();
 
-        let code_owners = CodeOwners::discover(docs_dir).expect("Should have found CODEOWNER file");
+        let code_owners = CodeOwners::discover(docs_dir)
+            .expect("Should have found CODEOWNER file")
+            .unwrap();
 
-        let owners = code_owners.owners().unwrap();
+        let owners = code_owners.owners();
 
         assert_eq!(
-            HashMap::from([("**/*.md".to_string(), vec!["@markdown".to_string()])]),
+            &HashMap::from([("**/*.md".to_string(), vec!["@markdown".to_string()])]),
             owners
         );
     }
@@ -168,26 +232,35 @@ mod tests {
         fs::write(dot_dir.join("CODEOWNERS"), "**/*.md @markdown").unwrap();
         fs::write(dot_dir.join("file.md"), "").unwrap();
 
-        let code_owners = CodeOwners::discover(dot_dir).expect("Should have found CODEOWNER file");
+        let code_owners = CodeOwners::discover(dot_dir)
+            .expect("Should have found CODEOWNER file")
+            .unwrap();
 
-        let owners = code_owners.owners().unwrap();
+        let owners = code_owners.owners();
 
         assert_eq!(
-            HashMap::from([("**/*.md".to_string(), vec!["@markdown".to_string()])]),
+            &HashMap::from([("**/*.md".to_string(), vec!["@markdown".to_string()])]),
             owners
         );
     }
 
-    // #[test]
-    // fn render_markdown() {
-    //     let (temp_dir, _tempdir_guard) = TempDirGuard::new().unwrap();
-    //
-    //     fs::write(temp_dir.join("CODEOWNERS"), "**/*.md @markdown").unwrap();
-    //     fs::write(temp_dir.join("file.md"), "").unwrap();
-    //
-    //     let code_owners = CodeOwners::discover(temp_dir).expect("Should have found CODEOWNER file");
-    //
-    //     let mut writer = Vec::<u8>::new();
-    //     let rendered = code_owners.render(&mut writer, "markdown");
-    // }
+    #[test]
+    fn render_markdown() {
+        let (temp_dir, _tempdir_guard) = TempDirGuard::new().unwrap();
+
+        fs::write(temp_dir.join("CODEOWNERS"), "**/*.md @markdown").unwrap();
+        fs::write(temp_dir.join("file.md"), "").unwrap();
+
+        let code_owners = CodeOwners::discover(temp_dir)
+            .expect("Should have found CODEOWNER file")
+            .unwrap();
+
+        let mut writer = Vec::<u8>::new();
+        let rendered = code_owners.render(&mut writer, "markdown").unwrap();
+
+        assert_eq!(
+            "| File(s) | Owners |\n|-|-|\n| **/*.md | @markdown |\n",
+            str::from_utf8(&writer).unwrap()
+        )
+    }
 }
